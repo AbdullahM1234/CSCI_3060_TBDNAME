cfd56beae7ef741ad1f96c2de2661166cccd2d63
Megan <90281084+mbrandreth@users.noreply.github.com>
2026-02-06 15:43:47 -0500

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,7 +1,7 @@
 # global variables
-username = "u"
-password = "p"
-session = "standard"
+name = "u" # default name
+session = "standard" # default session
+
 
 def login():
 
@@ -9,7 +9,14 @@ def login():
     # i.e if username does not equal u
 
     # if user is logged in, error message and redirect to log out
-
+    if username == "u":
+        print("You are already logged in. Logout?")
+        logAns = input("(y/n): ")
+        if logAns == "y" or logAns == "Y":
+            logout()
+        else:
+            return
+    
     # else
     print("Welcome.")
     print("What session are you opening today? (admin or standard)\n")
@@ -20,10 +27,11 @@ def login():
         session = "admin"
 
         # do not prompt for name input, move straight to menu
+        return
 
     else:
         print("standard selected.")
-        username = input("Name: ") 
+        name = input("Name: ") 
         # existing & valid account check
 
         # if not valid or not existing, prompt to create a new account or try again
@@ -40,12 +48,18 @@ def login():
 
         """
 
-        print(f"Welcome, {username}.")
+        print(f"Welcome, {name}.")
 
         # print account details
 
+    return
+
 def logout():
     # check that user is logged in (username not default)
+
+    if name == "u":
+        print("You are not logged in.")
+        return
     
     # print transactions made during session
 
@@ -61,177 +75,289 @@ def logout():
 def withdrawal():
     # check session and check login
 
+    if name == "u":
+        print("You are not logged in.")
+        return
+    
     # check if available for transactions
         # if not, prompt to log out 
 
     # if admin, ask for account holder name
-
-    # ask for account number
-    # check account number is valid
+    if session == "admin":
+        print("Please provide the account holder's name.")
+        accountName = input()
+    else:
+        accountName = name
+    # check that account name is valid
         # if not, prompt to try again
 
+    # ask for account number
+    print("Please provide the account number.")
+    accountNum = input()
+    # check account number is valid
+        # if not, prompt to try again
+    # check if account number is associated with account name
+        # if not, return
+
     # ask for  withdrawal amount
+    print("Please enter the withdrawal amount.")
+    withdraw = input()
 
     # check if more than $500
+    if withdraw >= 500:
+        #if true, prompt to try again
+        print("Sorry, this exceeds our withdraw limit of 500.\n Please try again.")
+        withdrawal()
+    
     # check that bank account will have at least $0 after transaction
         # if either true, reject withdrawal, return to menu
 
     # save to bank transaction file
 
-    print()
+    return
 
 def transfer():
     # check session and check login
+
+    if name == "u":
+        print("You are not logged in.")
+        return
 
     # check if available for transactions
         # if not, prompt to log out 
 
     # if admin, ask for account holder name
+    if session == "admin":
+        print("Please provide the account holder's name.")
+        accountName = input()
+    else:
+        accountName = name
+    # check that account name is valid
+    # if not, prompt to try again
 
     # ask for account number 1
+    print("Please provide the number for the account you are transfering from.")
+    acc1 = input()
     # check if valid
         # if not, prompt to try again
 
     # ask for account number 2
+    print("Please provide the number of the account you are transfering to.")
+    acc2 = input()
     # check if valid
         # if not, prompt to try again 
 
     # ask for transfer ammount
+    print("please provide a transfer amount.")
+    transferAmount = input()
 
     # check if more than $1000
+    if transferAmount >= 1000:
+        #if true, prompt to try again
+        print("Sorry, this exceeds our withdraw limit of 500.\n Please try again.")
+        transfer()
     # check if back account 1 will have at least $0 after transfer
     # check that account 2 is not overdrafted
         # if any true, reject transfer, return to menu
 
     # save to bank transaction file
-    print()
+
+    return
 
 def paybill():
     # check session and check login
+
+    if name == "u":
+        print("You are not logged in.")
+        return
 
     # check if available for transactions
         # if not, prompt to log out 
 
     # if admin, ask for account holder name
+    if session == "admin":
+        print("Please provide the account holder's name.")
+        accountName = input()
+    else:
+        accountName = name
 
     # ask for account number
+    print("Please provide an account number.")
+    accNum = input()
     # check if valid
         # if not, prompt to try again
 
     # ask for payee
+    print("Please provide the name of the Payee.")
+    payee = input()
     # check if valid payee
+    if payee != "The Bright Light Electric Company (EC)" or "Credit Card Company Q (CQ)" or "Fast Internet, Inc. (FI)":
         # if not, prompt to try again
+        print("Payee invalid. Try again.")
+        paybill()
 
     # ask for bill pay amount
+    print("Please enter the amount to be paid.")
+    billPaid = input()
 
     # check if more than $2000
+    if billPaid >= 2000:
+        print("This payment exceeds our payment limit. Please try again.")
+        paybill()
     # check if account will have at least $0 after payment
         # if either, reject payment and return to menu
 
     # save to bank transaction file
 
-    print()
+    return
+
 
 def deposit():
     # check session and check login
+
+    if name == "u":
+        print("You are not logged in.")
+        return
 
     # check if available for transactions
         # if not, prompt to log out 
 
     # if admin, ask for account holder name
+    if session == "admin":
+        print("Please provide the account holder's name.")
+        accountName = input()
+    else:
+        accountName = name
 
     # ask for account number
+    print("Please provide an account number.")
+    accNum = input()
     # check if valid
         # if not, prompt to try again
     
     # ask for deposit amount
+    print("Please provide the amount to be deposited.")
+    deposit = input()
 
     # save to bank transaction file
 
     # store to be added at end of session
-    print()
+
+    return
 
 def create():
     # check session type, only accept if admin
 
-    # else, return
-
+    if name == "u":
+        print("You are not logged in.")
+        return 
     # ask for name
-    # check if exists
-        # if it does, prompt to try again
-    # check if name is more than 20 characters
-        # if it is, prompt to try again
+    if session == "admin":
+        print("Please provide the account holder's name.")
+        accountName = input()
+        # check if exists
+            # if it does, prompt to try again
+        # check if name is more than 20 characters
+        if len(accountName) > 20:
+            # if it is, prompt to try again
+            print("Name provided is too long. Try again.")
+            create()
+            
 
-    # ask for inital account balance
-    # check if less than $99999.99
-        # if not, prompt to try again
+        # ask for inital account balance
+        print("Please provide initial account balance.")
+        balanceinit = input()
+        # check if less than $99999.99
+        if balanceinit >= 100000:
+            # if not, prompt to try again
+            print("Balance exceeds limit. Please try again.")
+            create()
 
-    # generate random account number
+        # generate random account number
 
-    # save and print info to bank transaction file
+        # save and print info to bank transaction file
 
-    # note to not allow transactions on this account until session ends
-    print()
+        # note to not allow transactions on this account until session ends
+        # else, return
+    else:
+        return
+    return
 
 def delete():
     # check session type, only accept if admin
 
+    if session == "admin":
+        # ask for name
+        print("Please provide the account holder's name.")
+        accountName = input()
+        # check if exists
+            # if not, prompt to try again
+            
+        # ask for account number
+        print("Please provide an account number.")
+        accNum = input()
+        # check if exists
+            # if not, prompt to try again
+        # check if associated with name
+            # if not, return to menu
+            
+        # save to bank transaction file
+
+        # set data to deleted
     # else, return
-
-    # ask for name
-    # check if exists
-        # if not, prompt to try again
-    
-    # ask for account number
-    # check if exists
-        # if not, prompt to try again
-    # check if associated with name
-        # if not, return to menu
-    
-    # save to bank transaction file
-
-    # set data to deleted
-
-    print()
+    else:
+        return
+    return
 
 def disable():
     # check session type, only accept if admin
-
-    # else, return
-
+    if session == "admin":
     # ask for name
-    # check if exists
-        # if not, prompt to try again
+        print("Please provide the account holder's name.")
+        accountName = input()
+        # check if exists
+            # if not, prompt to try again
 
-    # ask for account number
-    # check if exists
-        # if not, prompt to try again
-    # check if associated with name
-        # if not, return to menu
-    
-    # change bank account status to disabled and no transactions
-    # save to bank account transaction file
-    print()
+        # ask for account number
+        print("Please provide an account number.")
+        accNum = input()
+        # check if exists
+            # if not, prompt to try again
+        # check if associated with name
+            # if not, return to menu
+            
+        # change bank account status to disabled and no transactions
+        # save to bank account transaction file
+    # else, return
+    else:
+        return
+    return
 
 def changeplan():
     # check session tupe, only accept if admin
+    if session == "admin":
+        # ask for name
+        print("Please provide the account holder's name.")
+        accountName = input()
+        # check if exists
+            # if not, prompt to try again
 
+        # ask for account number
+        print("Please provide an account number.")
+        accNum = input()
+        # check if exists
+            # if not, prompt to try again
+        # check if associated with name
+            # if not, return to menu
+        
+        # change bank account plan to either student or non student (opposite whatever is currently selected)
+
+        # save to bank account transaction file
     # else, return
-
-    # ask for name
-    # check if exists
-        # if not, prompt to try again
+    else:
+        return
     
-    # ask for account number
-    # check if exists
-        # if not, prompt to try again
-    # check if associated with name
-        # if not, return to menu
-    
-    # change bank account plan to either student or non student (opposite whatever is currently selected)
-
-    # save to bank account transaction file
-    print()
+    return
 
 def main():
     login()
